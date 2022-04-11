6636e1227b1265170bb356f1ff86f3d1400ba800
Vinay Shrestha <svinay@insightworkshop.io>
2022-04-11 11:18:39 +0545

    Modify forgot password response message

--- src/config/strings.ts
+++ src/config/strings.ts
@@ -61,4 +61,5 @@ export default {
   UserIdRequired: 'User Id required',
   userTypeRequired: 'User type required',
   tokenExpired: 'Token expired',
+  forgotPasswordMsg: 'If the email exists in our system, a token will be sent to reset your password.',
 };
