4b04aa990e83c3f683e8b36f0fcbab21e760d6e2
Vinay Shrestha <vinayshrestha555@gmail.com>
2022-06-08 18:19:20 +0545

    Update seed

--- src/config/db/seeds/initial-seed.ts
+++ src/config/db/seeds/initial-seed.ts
@@ -1,6 +1,7 @@
 import find from 'lodash/find';
 import { Connection } from 'typeorm';
 import { Factory, Seeder } from 'typeorm-seeding';
+import { faker } from '@faker-js/faker';
 
 import constants from '../../constants';
 
@@ -9,8 +10,17 @@ import Role from '../../../entities/role.entity';
 import Company from '../../../entities/company.entity';
 import Address from '../../../entities/address.entity';
 import Client from '../../../entities/client.entity';
+import Project from '../../../entities/project.entity';
+import Task from '../../../entities/task.entity';
 import UserClient from '../../../entities/user-client.entity';
-import { Role as RoleEnum, CompanyStatus, ClientStatus, UserClientStatus } from '../../../config/constants';
+import {
+  Role as RoleEnum,
+  CompanyStatus,
+  ClientStatus,
+  UserClientStatus,
+  ProjectStatus,
+  TaskStatus,
+} from '../../../config/constants';
 
 const roles = [
   {
@@ -81,6 +91,15 @@ const address = {
   zipCode: '75001',
 };
 
+const projects: any = [];
+for (let i = 0; i < 10; i++) {
+  const project = {
+    name: faker.company.companyName(),
+    status: ProjectStatus.Active,
+  };
+  projects.push(project);
+}
+
 export default class InitialDatabaseSeed implements Seeder {
   public async run(factory: Factory, connection: Connection): Promise<void> {
     let createdRoles: any = [];
@@ -109,6 +128,7 @@ export default class InitialDatabaseSeed implements Seeder {
 
         createdCompanies.push(newCompany);
       }
+      const company_id = createdCompanies[0].id;
 
       const createdAddress = await factory(Address)().create(address);
 
@@ -122,19 +142,49 @@ export default class InitialDatabaseSeed implements Seeder {
         createdClients.push(newClient);
       }
 
+      const client_id = createdClients[0].id;
+      const createdUsers: User[] = [];
+      let employee: User | undefined = undefined;
+      let manager: User | undefined = undefined;
+
       for (let i = 1; i < users.length; i++) {
         const newUser = await factory(User)().create({
           roles: [createdRoles[i]],
           email: users[i].email,
-          company_id: createdCompanies[0]?.id,
+          company_id,
         });
 
-        // Associate client only for employee
-        if (find(newUser?.roles ?? [], { name: 'Employee' })) {
+        createdUsers.push(newUser);
+
+        if (find(newUser?.roles ?? [], { name: 'TaskManager' })) {
+          manager = newUser;
+        } else if (find(newUser?.roles ?? [], { name: 'Employee' })) {
+          // Associate client only for employee
+          employee = newUser;
           await factory(UserClient)().create({
             status: UserClientStatus.Active,
             user_id: newUser.id,
-            client_id: createdClients[0].id,
+            client_id,
+          });
+        }
+      }
+
+      for (let i = 0; i < projects.length; i++) {
+        const newProject = await factory(Project)().create({
+          ...projects[i],
+          company_id,
+          client_id,
+        });
+
+        for (let j = i; j < i + 4; j++) {
+          await factory(Task)().create({
+            name: faker.company.catchPhrase(),
+            description: faker.git.commitMessage(),
+            status: TaskStatus.Scheduled,
+            company_id,
+            manager_id: manager?.id,
+            project_id: newProject.id,
+            users: employee ? [employee] : [],
           });
         }
       }
